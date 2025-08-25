56f56dc4319ee1c473d78e758a9322ffbf98ea23
Jaime Mejia <jaimemejia1003@gmail.com>
2025-08-25 18:43:25 -0500

    Refactor main application structure by moving the entry point to a new main.py file and removing it from app/main.py

--- app/main.py
+++ app/main.py
@@ -16,8 +16,3 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
